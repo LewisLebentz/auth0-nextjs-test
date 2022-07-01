cb498dcec50fd2bfd299c71dc1c45eecc7ba95c7
Lewis Lebentz <LewisLebentz@users.noreply.github.com>
2022-07-01 20:59:41 +0100

    Update shows.js

--- Sample-01/pages/api/shows.js
+++ Sample-01/pages/api/shows.js
@@ -1,20 +1,16 @@
 import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
 
 export default withApiAuthRequired(async function shows(req, res) {
-  try {
-    const { accessToken } = await getAccessToken(req, res, {
-      scopes: ['read:shows']
-    });
-    const apiPort = process.env.API_PORT || 3001;
-    const response = await fetch(`https://auth0-nextjs-test.vercel.app/api/shows`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    });
-    const shows = await response.json();
-
-    res.status(200).json(shows);
-  } catch (error) {
-    res.status(error.status || 500).json({ error: error.message });
-  }
+  // If your Access Token is expired and you have a Refresh Token
+  // `getAccessToken` will fetch you a new one using the `refresh_token` grant
+  const { accessToken } = await getAccessToken(req, res, {
+    scopes: ['read:products']
+  });
+  const response = await fetch('https://auth0-nextjs-test.vercel.app/api/shows', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  });
+  const products = await response.json();
+  res.status(200).json(products);
 });
