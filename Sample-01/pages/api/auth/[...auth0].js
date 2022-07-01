3509e2fd94c25ffacb9dd28996299a1584eded6a
Lewis Lebentz <LewisLebentz@users.noreply.github.com>
2022-07-01 20:56:00 +0100

    Update [...auth0].js

--- Sample-01/pages/api/auth/[...auth0].js
+++ Sample-01/pages/api/auth/[...auth0].js
@@ -1,3 +1,20 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+const audience = process.env.AUTH0_AUDIENCE;
+const scope = process.env.AUTH0_SCOPE;
+
+export default handleAuth({
+  async login(req, res) {
+    try {
+      await handleLogin(req, res, {
+        authorizationParams: {
+          audience: audience, // or AUTH0_AUDIENCE
+          // Add the `offline_access` scope to also get a Refresh Token
+          scope: scope // or AUTH0_SCOPE
+        }
+      });
+    } catch (error) {
+      res.status(error.status || 400).end(error.message);
+    }
+  }
+});
